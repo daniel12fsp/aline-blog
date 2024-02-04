69fe2c4404c3c71518ef90b277b6a563313407d8
daniel12fsp <daniel12fsp@yahoo.com.br>
2024-02-04 18:44:43 +0000

    Deploying to gh-pages from @ daniel12fsp/aline-blog@f0bac67f78edb26f353abb7e878143c450ef4f14 🚀

--- _next/static/tcRUziJ2woKE29hpjeupf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/804-37f5eefcf12f4a93.js","static/css/663422b7d5d06e33.css","static/chunks/pages/index-2d0e7b085edbf703.js"],"/_error":["static/chunks/pages/_error-0cb2bff341a47607.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
